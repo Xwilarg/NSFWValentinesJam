8e86618feb019460a236b2de19f42be66a6aeed8
Xwilarg <xwilarg@yahoo.fr>
2019-02-08 15:59:00 +0100

    Add AI script

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -1,5 +1,25 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class AI : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private const float speed = 100f;
+    private const float maxDist = 2f;
+
+    private bool goLeft;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        goLeft = true;
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(((goLeft) ? -1 : 1) * speed * Time.deltaTime, rb.velocity.y);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(((goLeft) ? -1 : 1), 0f), maxDist, 1 << 10);
+        if (hit.distance > 0f)
+            goLeft = !goLeft;
+    }
 }
