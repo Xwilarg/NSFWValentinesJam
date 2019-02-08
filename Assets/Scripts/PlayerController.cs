9c68767fcdb40edcfb70d4996328ba2e397173f1
Xwilarg <xwilarg@yahoo.fr>
2019-02-08 11:41:04 +0100

    Add ability to hide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,39 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
     private const float speed = 300f;
+
+    [SerializeField]
+    private Sprite hiddenSprite, baseSprite;
+
+    private bool isHidding;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        isHidding = false;
     }
 
     private void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * speed, rb.velocity.y);
+        if (!isHidding)
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * speed, rb.velocity.y);
+        if (Input.GetButtonDown("Hide"))
+        {
+            sr.sprite = hiddenSprite;
+            rb.velocity = Vector2.zero;
+            isHidding = true;
+        }
+        else if (Input.GetButtonUp("Hide"))
+        {
+            sr.sprite = baseSprite;
+            isHidding = false;
+        }
     }
 }
