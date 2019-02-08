1f1161da6e9b6f9fa35dd3e1527f747549869f12
Xwilarg <xwilarg@yahoo.fr>
2019-02-08 16:20:21 +0100

    Fix light

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -20,6 +20,9 @@ public class AI : MonoBehaviour
         rb.velocity = new Vector2(((goLeft) ? -1 : 1) * speed * Time.deltaTime, rb.velocity.y);
         RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(((goLeft) ? -1 : 1), 0f), maxDist, 1 << 10);
         if (hit.distance > 0f)
+        {
             goLeft = !goLeft;
+            transform.localScale *= -1;
+        }
     }
 }
---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,27 @@
+﻿public static class Constants
+{
+    public static readonly Cloth SchoolSwimsuit = new Cloth("School Swimsuit", .4f);
+    public static readonly Cloth Panty = new Cloth("School Swimsuit", .1f);
+    public static readonly Cloth Socks = new Cloth("School Swimsuit", .1f);
+    public static readonly Cloth Tights = new Cloth("School Swimsuit", .2f);
+    public static readonly Cloth Skirt = new Cloth("School Swimsuit", .3f);
+    public static readonly Cloth Throusers = new Cloth("School Swimsuit", .5f);
+    public static readonly Cloth Vest = new Cloth("School Swimsuit", .5f);
+
+    public static readonly Cloth[] clothes = new Cloth[]
+    {
+        SchoolSwimsuit, Panty, Socks, Tights, Skirt, Throusers, Vest
+    };
+
+    public struct Cloth
+    {
+        public Cloth(string name, float value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { private set; get; }
+        public float Value { private set; get; }
+    }
+}
