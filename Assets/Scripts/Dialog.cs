1771a7ab24a5e988fb9a27cc4f73fe846e0efcd2
Xwilarg <xwilarg@yahoo.fr>
2019-02-08 17:19:42 +0100

    Add dialog box

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour
+{
+    [SerializeField]
+    private Text nameText, dialogText;
+    
+    public void Enable(string name, string content)
+    {
+        nameText.enabled = true;
+        dialogText.enabled = true;
+        nameText.text = name;
+        dialogText.text = content;
+    }
+
+    private void Update()
+    {
+        if (nameText.enabled && Input.anyKeyDown)
+        {
+            nameText.enabled = false;
+            dialogText.enabled = false;
+        }
+    }
+}
