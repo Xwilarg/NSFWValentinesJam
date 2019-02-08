834c45610e3054c6d62012107d5de451d65cd48a
Xwilarg <xwilarg@yahoo.fr>
2019-02-08 13:02:58 +0100

    Improve hide method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,41 +25,27 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         if (!isHidding)
+        {
             rb.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * speed, rb.velocity.y);
+            if (Input.GetButtonDown("Interract") && currentDoor != null)
+            {
+                Transform output = currentDoor.GetComponent<Door>().GetDoorTransform();
+                transform.position = output.position;
+                transform.parent = output.parent.parent;
+            }
+        }
         if (Input.GetButtonDown("Hide"))
         {
             sr.sprite = hiddenSprite;
             rb.velocity = Vector2.zero;
             isHidding = true;
-            ActionOnChilds(transform.parent, false);
+            transform.parent.GetComponent<RoomManager>().DisableAll();
         }
         else if (Input.GetButtonUp("Hide"))
         {
             sr.sprite = baseSprite;
             isHidding = false;
-            ActionOnChilds(transform.parent, true);
-        }
-        if (Input.GetButtonDown("Interract") && currentDoor != null)
-        {
-            Transform output = currentDoor.GetComponent<Door>().GetDoorTransform();
-            transform.position = output.position;
-            transform.parent = output.parent.parent;
-        }
-    }
-
-    private void ActionOnChilds(Transform parent, bool state)
-    {
-        foreach (Transform t in parent)
-        {
-            if (t.CompareTag("Player"))
-                continue;
-            if (t.childCount > 0)
-                ActionOnChilds(t, state);
-            else
-            {
-                Renderer renderer = t.GetComponent<Renderer>();
-                renderer.enabled = state;
-            }
+            transform.parent.GetComponent<RoomManager>().EnableAll();
         }
     }
 
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RoomManager : MonoBehaviour
+{
+    private List<TilemapRenderer> tilemapRenderers;
+    private List<SpriteRenderer> spriteRenderers;
+
+    private float timer;
+
+    private void Start()
+    {
+        tilemapRenderers = new List<TilemapRenderer>();
+        spriteRenderers = new List<SpriteRenderer>();
+        SaveChildren(transform);
+        timer = -1f;
+    }
+
+    private void Update()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            SetColor(timer);
+        }
+    }
+
+    private void SaveChildren(Transform parent)
+    {
+        foreach (Transform t in parent)
+        {
+            if (t.CompareTag("Player"))
+                continue;
+            if (t.childCount > 0)
+                SaveChildren(t);
+            else
+            {
+                Renderer renderer = t.GetComponent<Renderer>();
+                if (renderer != null)
+                {
+                    TilemapRenderer tr = renderer as TilemapRenderer;
+                    if (tr != null)
+                        tilemapRenderers.Add(tr);
+                    else
+                        spriteRenderers.Add((SpriteRenderer)renderer);
+                }
+            }
+        }
+    }
+
+    private void SetColor(float color)
+    {
+        foreach (var v in tilemapRenderers)
+            v.material.color = new Color(v.material.color.r, v.material.color.g, v.material.color.b, color);
+        foreach (var v in spriteRenderers)
+            v.color = new Color(v.material.color.r, v.material.color.g, v.material.color.b, color);
+    }
+
+    public void DisableAll()
+    {
+        timer = 1f;
+    }
+
+    public void EnableAll()
+    {
+        timer = -1f;
+        SetColor(1f);
+    }
+}
