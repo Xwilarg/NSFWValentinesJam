e793c5bcf92ca7e5b41aac31e1aa1dc9ddd9365f
Xwilarg <xwilarg@yahoo.fr>
2019-02-08 13:38:17 +0100

    Add wardrobe sprites

---
+++ Assets/Scripts/Wardrobe.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Wardrobe : MonoBehaviour
+{
+    private bool isEmpty;
+
+    private SpriteRenderer sr;
+
+    [SerializeField]
+    private Sprite spriteEmpty, spriteGhost;
+
+    private void Start()
+    {
+        isEmpty = false;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Enter()
+    {
+        sr.sprite = spriteGhost;
+        isEmpty = true;
+    }
+
+    private void Exit()
+    {
+        sr.sprite = spriteEmpty;
+    }
+}
