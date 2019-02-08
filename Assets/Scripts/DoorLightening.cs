55c7cfdd05abb52ae1ff21964f07c00f0ae6aeab
Xwilarg <xwilarg@yahoo.fr>
2019-02-08 23:54:08 +0100

    Add beginning of "Door Light"

---
+++ Assets/Scripts/DoorLightening.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLightening : MonoBehaviour
+{
+    private List<GameObject> closeGos;
+
+    private void Start()
+    {
+        closeGos = new List<GameObject>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("AI"))
+            closeGos.Add(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("AI"))
+            closeGos.Remove(collision.gameObject);
+    }
+}
