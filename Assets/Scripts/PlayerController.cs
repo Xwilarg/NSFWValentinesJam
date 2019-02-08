f0eeba830bfa7a4f41a9cbe54deb90835c7ee7e8
Xwilarg <xwilarg@yahoo.fr>
2019-02-08 11:26:30 +0100

    Add player movements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private const float speed = 300f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * speed, rb.velocity.y);
+    }
+}
