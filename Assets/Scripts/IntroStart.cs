1d354009a10bb21bc7162fd0250d688a044cd787
Max <vollat.maxime@wanadoo.fr>
2019-02-09 16:31:21 +0100

    Add sounds and voice manager

--- Assets/Scripts/IntroStart.cs
+++ Assets/Scripts/IntroStart.cs
@@ -3,10 +3,13 @@
 public class IntroStart : MonoBehaviour
 {
     [SerializeField]
+    public AudioSource source;
+    [SerializeField]
     private Dialog dialog;
 
     private void Start()
     {
+        dialog.source = source;
         dialog.Launch();
     }
 }
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sound
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public AudioClip[] audioClips;
+        [SerializeField]
+        public Dictionary<string, AudioClip> sounds;
+
+        private void Start()
+        {
+            sounds.Add("breathing", audioClips[0]);
+            sounds.Add("dmgClothe", audioClips[1]);
+            sounds.Add("dedStudent", audioClips[2]);
+            sounds.Add("dedTeacher", audioClips[3]);
+            sounds.Add("doorLocked", audioClips[4]);
+            sounds.Add("doorOpen", audioClips[5]);
+            sounds.Add("eatClothe", audioClips[6]);
+            sounds.Add("evilDoor", audioClips[7]);
+            sounds.Add("lockedDoorOpen", audioClips[8]);
+            sounds.Add("dmgSoul", audioClips[9]);
+            sounds.Add("menuBack", audioClips[10]);
+            sounds.Add("menuConfirm", audioClips[11]);
+            sounds.Add("footstep", audioClips[12]);
+            sounds.Add("intro", audioClips[13]);
+            sounds.Add("end1", audioClips[14]);
+            sounds.Add("end2", audioClips[15]);
+            sounds.Add("end3", audioClips[16]);
+            sounds.Add("duoCG", audioClips[17]);
+        }
+    }
+}
