547dd91358d7db1ed6cb6c753a0a3255573000cf
Xwilarg <xwilarg@yahoo.fr>
2019-02-09 11:29:06 +0100

    Add meta and translation base

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -21,7 +21,54 @@
             Value = value;
         }
 
+
         public string Name { private set; get; }
         public float Value { private set; get; }
+    }
+
+    public enum LanguageName
+    {
+        fr,
+        en,
+        nl
+    }
+
+    public static string GetLine(string key, LanguageName language)
+    {
+        Language? lang = System.Array.Find(lines, x => x.Key == key);
+        if (lang == null)
+            return ("Translation error: Key " + key + " not found.");
+        return (lang.Value.GetLine(language));
+    }
+
+    static Language[] lines = new Language[]
+    {
+    };
+
+    public struct Language
+    {
+        public Language(string key, string frenchText, string englishText, string dutchText)
+        {
+            Key = key;
+            FrenchText = frenchText;
+            EnglishText = englishText;
+            DutchText = dutchText;
+        }
+
+        public string GetLine(LanguageName language)
+        {
+            if (language == LanguageName.en)
+                return (EnglishText);
+            if (language == LanguageName.fr)
+                return (FrenchText);
+            if (language == LanguageName.nl)
+                return (DutchText);
+            return (null);
+        }
+
+        public string Key { private set; get; }
+        public string FrenchText { private set; get; }
+        public string EnglishText { private set; get; }
+        public string DutchText { private set; get; }
     }
 }
