7c3cc0603dce6bc5ea9dfd2af1d972c4793c5ed7
Xwilarg <xwilarg@yahoo.fr>
2019-02-09 10:14:24 +0100

    Add door lightening

--- Assets/Scripts/DoorLightening.cs
+++ Assets/Scripts/DoorLightening.cs
@@ -5,9 +5,16 @@ public class DoorLightening : MonoBehaviour
 {
     private List<GameObject> closeGos;
 
+    DoorLightening otherDoor;
+
+    [SerializeField]
+    private SpriteRenderer yellowLight, purpleLight;
+
     private void Start()
     {
         closeGos = new List<GameObject>();
+        int tmp = 1;
+        otherDoor = transform.parent.GetComponent<Door>().GetNextDoor(ref tmp).GetComponentInChildren<DoorLightening>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -20,5 +27,51 @@ public class DoorLightening : MonoBehaviour
     {
         if (collision.CompareTag("AI"))
             closeGos.Remove(collision.gameObject);
+    }
+
+    private void Update()
+    {
+        if (closeGos.Count == 0)
+        {
+            otherDoor.LightNone();
+            return;
+        }
+        GameObject closest = null;
+        float distance = float.MaxValue;
+        foreach (GameObject go in closeGos)
+        {
+            float currDist = Vector2.Distance(transform.position, go.transform.position);
+            if (closest == null || currDist < distance)
+            {
+                closest = go;
+                distance = currDist;
+            }
+        }
+        distance = Mathf.Clamp(distance, 0f, 10f);
+        distance = 1f - distance / 10f;
+        if (closest.GetComponent<AI>() != null)
+            otherDoor.LightYellow(distance);
+        else
+            otherDoor.LightPurple(distance);
+    }
+
+    public void LightNone()
+    {
+        yellowLight.gameObject.SetActive(false);
+        purpleLight.gameObject.SetActive(false);
+    }
+
+    public void LightYellow(float intensity)
+    {
+        yellowLight.gameObject.SetActive(true);
+        yellowLight.color = new Color(yellowLight.color.r, yellowLight.color.g, yellowLight.color.b, intensity);
+        purpleLight.gameObject.SetActive(false);
+    }
+
+    public void LightPurple(float intensity)
+    {
+        yellowLight.gameObject.SetActive(false);
+        purpleLight.gameObject.SetActive(true);
+        purpleLight.color = new Color(purpleLight.color.r, purpleLight.color.g, purpleLight.color.b, intensity);
     }
 }
