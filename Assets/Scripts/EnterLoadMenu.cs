35040fb35b0d77d12c3d942cb579df1df48f1c54
Xwilarg <xwilarg@yahoo.fr>
2019-02-10 11:30:12 +0100

    Add main menu and warning screen

---
+++ Assets/Scripts/EnterLoadMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterLoadMenu : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+            SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
