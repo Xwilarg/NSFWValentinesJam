950bb8b13b2a2d41da995b59455746d7e1842d2c
Xwilarg <xwilarg@yahoo.fr>
2019-02-11 05:56:23 +0100

    Add rotate screen

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private const float speed = 3f;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, Time.deltaTime * speed));
+    }
+}
