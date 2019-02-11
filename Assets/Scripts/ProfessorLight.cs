f90209bc564518fd2bc3b7e18d567659ddb79e96
Xwilarg <xwilarg@yahoo.fr>
2019-02-11 05:41:15 +0100

    Fix AI behaviour

--- Assets/Scripts/ProfessorLight.cs
+++ Assets/Scripts/ProfessorLight.cs
@@ -4,31 +4,40 @@ public class ProfessorLight : MonoBehaviour
     private PlayerController pc;
     private AI ai;
 
+    private bool takeDamage;
+
     private void Start()
     {
         pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         ai = transform.parent.GetComponent<AI>();
+        takeDamage = false;
+    }
+
+    private void Update()
+    {
+        if (takeDamage)
+            pc.TakeDamage();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && ai != null)
-            ai.isMoving = false;
+        if (collision.CompareTag("Player"))
+        {
+            if (ai != null)
+                ai.isMoving = false;
+            pc = collision.GetComponent<PlayerController>();
+            if (!pc.IsHidden() && !pc.IsInWardrobe())
+                takeDamage = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && ai != null)
-            ai.isMoving = true;
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
         if (collision.CompareTag("Player"))
         {
-            PlayerController pc = collision.GetComponent<PlayerController>();
-            if (!pc.IsHidden() && !pc.IsInWardrobe())
-                pc.TakeDamage();
+            if (ai != null)
+                ai.isMoving = true;
+            takeDamage = false;
         }
     }
 }
