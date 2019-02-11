e128e8fb912cbc6c29822286cd6a48c29562812d
Xwilarg <xwilarg@yahoo.fr>
2019-02-11 04:33:16 +0100

    Add load victory

---
+++ Assets/Scripts/LoadVictory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadVictory : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("GoodEnding");
+    }
+}
